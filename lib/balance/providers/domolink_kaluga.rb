80ef8348cc38d45e53d39a1e4d600435f089af4f
Alexey Noskov <alexey.noskov@gmail.com>
2011-03-05 22:45:05 +0300

    Domolink Kaluga

---
+++ lib/balance/providers/domolink_kaluga.rb
@@ -0,0 +1,20 @@
+require 'mechanize'
+
+class Balance::Providers::DomolinkKaluga
+
+  def check( login, password )
+    agent = Mechanize.new
+
+    login_page = agent.get 'http://cabinet.kaluga.ru/klg/www.PageViewer?page_name=S*START_PAGE'
+    login_form = login_page.forms.first
+    login_form.field(:name => 'p_logname').value = login.upcase
+    login_form.field(:name => 'p_pwd').value = password
+
+    result_page = agent.get login_form.submit.meta.first.href
+
+    result_page.parser.css('.dat').text.match(/\d+\.\d+/)[0].to_f
+  end
+
+end
+
+Balance.add_provider! 'domolink_kaluga', Balance::Providers::DomolinkKaluga
