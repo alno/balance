a51f513e6478e5b80290854f1cca4f4f8b07710b
Alexey Noskov <alexey.noskov@gmail.com>
2012-11-18 13:08:11 +0400

    Fix domolink_kaluga

--- lib/balance/providers/domolink_kaluga.rb
+++ lib/balance/providers/domolink_kaluga.rb
@@ -4,14 +4,14 @@ class Balance::Providers::DomolinkKaluga
 
   def check( login, password )
     agent = Mechanize.new
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-    login_page = agent.get 'http://cabinet.kaluga.ru/klg/www.PageViewer?page_name=S*START_PAGE'
+    login_page = agent.get 'https://room.centertelecom.ru/klg/www.PageViewer?page_name=S*START_PAGE'
     login_form = login_page.forms.first
     login_form.field(:name => 'p_logname').value = login.upcase
     login_form.field(:name => 'p_pwd').value = password
 
-    result_page = agent.get login_form.submit.meta.first.href
-
+    result_page = login_form.submit
     result_page.parser.css('.dat').text.match(/\d+\.\d+/)[0].to_f
   end
 
